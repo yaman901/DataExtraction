11ba434e89b9f9974d29e5ab4bc9fd8058079470
YAMANSHARMA\asus <yamansharma63@gmail.com>
2026-01-17 03:01:38 +0530

    Console app - to extract data from the images

---
+++ ExtractData/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tesseract;
+
+var imagePath = "C:/Users/asus/Downloads/Screenshot_1.png";
+
+using var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default);
+using var img = Pix.LoadFromFile(imagePath);
+using var page = engine.Process(img);
+
+string text = page.GetText();
+Console.WriteLine("Extracted Text:");
+Console.WriteLine(text);
